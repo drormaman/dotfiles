96167bb9d85d996ccc128ca791dcb33b2b601f06
Dror Maman <drormaman@gmail.com>
2024-04-21 10:57:38 +0300

    nvchad first commit

--- .config/nvim/lua/custom/plugins/line-blame.lua
+++
@@ -1,6 +0,0 @@
-return {
-  'tveskag/nvim-blame-line',
-  config = function ()
-    vim.keymap.set('n', '<leader>gb', '<cmd>ToggleBlameLine<CR>', {desc = '[G]it line [B]lame'})
-  end
-}
--- .config/nvim/lua/custom/plugins/nvim-tmux-navigator.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "christoomey/vim-tmux-navigator",
-  -- vim.keymap.set('n', 'c-h', ':TmuxNavigateLeft'),
-  -- vim.keymap.set('n', 'c-k', ':TmuxNavigateUp'),
-  -- vim.keymap.set('n', 'c-j', ':TmuxNavigateDown'),
-  -- vim.keymap.set('n', 'c-l', ':TmuxNavigateRight'),
-}
